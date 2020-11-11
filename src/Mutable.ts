06be99c18f72e55367c1f3e1a06d61225a497b3f
lemanh-tuong <letuong05092000@gmail.com>
2020-11-11 11:45:48 +0700

    add Mutable + Readonly + add folder Curry

---
+++ src/Mutable.ts
@@ -0,0 +1,25 @@
+export type Mutable<T> = {
+  -readonly [K in keyof T]: T[K]
+}
+
+const arrA = [1, 2, 3, 4] as const;
+const arrB = [1, [2, [3, [4]]]] as const;
+const case1: Mutable<typeof arrA> = [1, 2, 3, 4]; // Result: [1, 2, 3, 4]
+const case2: Mutable<typeof arrB> = [1, [2, [3, [4]]]] // Result: [1, readonly [2, readonly [3, readonly [4]]]]
+
+
+const objA = { name: 'Tuong', age: 20 } as const;
+const objB = { name: 'Tuong', age: 20, data: { id: '123', anotherProperty: false } } as const;
+
+const case3: Mutable<typeof objA> = { age: 20, name: 'Tuong' } // Result: { age: 20, name: 'Tuong' }
+const case4: Mutable<typeof objB> = { age: 20, name: 'Tuong', data: { anotherProperty: false, id: '123' } };
+// Result: { age: 20, name: 'Tuong', data: { readonly id: "123"; readonly anotherProperty: false; }}
+
+
+const a = [1, [2, 3, [4]]] as const;
+const b = [1, [2, [3, [4]]]] as const;
+
+export type DeepMutable<T> = { -readonly [P in keyof T]: DeepMutable<T[P]> }; 
+
+const caseDeep1: DeepMutable<typeof a> = [1, [2, 3, [4]]]; // Result: [1, [2, 3, [4]]]
+const caseDeep2: DeepMutable<typeof b> = [1, [2, [3, [4]]]] // Result: [1, [2, [3, [4]]]]
