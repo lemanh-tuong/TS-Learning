a56bd133acce337a85138c1cd3bbfc3864c912cc
lemanh-tuong <55147271+lemanh-tuong@users.noreply.github.com>
2020-12-01 22:10:16 +0700

    add PathOfObject

--- src/MergeObject.ts
+++ src/MergeObject.ts
@@ -1,13 +1,13 @@
 export type MergeObject<A extends object, B extends object> = Omit<A, keyof B> & B; 
 
-// type X  = {
-//     a: 1
-//     b: number
-// }
-// type Y = {
-//     a: 2
-//     b: string
-//     c: boolean
-// }
-// // XY is never, as field 'a' evaluates as 1 & 2 which is never
-// type XY = X & Y 
+type X  = {
+    a: 1
+    b: number
+}
+type Y = {
+    a: 2
+    b: string
+    c: boolean
+}
+// XY is never, as field 'a' evaluates as 1 & 2 which is never
+type XY = MergeObject<X, Y> // Result: {a: 2, b: string, c: boolean}
--- src/TS4.1/Join.ts
+++ src/TS4.1/Join.ts
@@ -1,5 +1,5 @@
-// https://github.com/microsoft/TypeScript/pull/40336
-// type Join<T extends unknown[], D extends string> =
+// // https://github.com/microsoft/TypeScript/pull/40336
+// export type Join<T extends unknown[], D extends string> =
 //     T extends [] ? '' :
 //     T extends [string | number | boolean | bigint] ? `${T[0]}` :
 //     T extends [string | number | boolean | bigint, ...infer U] ? `${T[0]}${D}${Join<U, D>}` :
@@ -7,5 +7,3 @@
 // type T30 = Join<[1, 2, 3, 4], '.'>;  // '1.2.3.4'
 // type T31 = Join<['foo', 'bar', 'baz'], '-'>;  // 'foo-bar-baz'
 // type T32 = Join<[], '.'>;  // ''
-
-// Ts4.1
