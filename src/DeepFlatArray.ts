c5471d86c2a84ada563eeafa72c6dc152df28c18
lemanh-tuong <55147271+lemanh-tuong@users.noreply.github.com>
2020-12-03 20:07:16 +0700

    move DeepMutable to new file + add FlatArray & DeepFlatArray

---
+++ src/DeepFlatArray.ts
@@ -0,0 +1,26 @@
+import { FlatArray } from './FlatArray';
+
+// https://github.com/microsoft/TypeScript/issues/40250
+// (length của array [-1, 0, 1, 2, 3, ...] bắt buộc > Depth)
+// "Deepth extends number" là độ sâu có thể flat
+type DeepFlatArray<Arr, Depth extends number> = {
+    0: Arr extends unknown[]
+        ? DeepFlatArray<FlatArray<Arr>, [-1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24][Depth]>
+        : Arr,
+    1: Arr
+}[Depth extends 0 ? 1 : 0];
+
+// Nó chạy đc đấy. Đừng đùa 😃 😃 😃
+
+type Case1 = DeepFlatArray< [
+    [
+        'a',
+        'b',
+    ] ,
+    [
+        'c',
+        'd',
+    ] ,
+    ['e', ['f', ['g'], ['h']  ]  ] 
+], 10>;
+
--- src/Mutable.ts
+++ src/Mutable.ts
@@ -16,25 +16,3 @@ const case3: Mutable<typeof objA> = { age: 20, name: 'Tuong' } // Result: { age:
 const case4: Mutable<typeof objB> = { age: 20, name: 'Tuong', data: { anotherProperty: false, id: '123' } }; // Result: { age: 20, name: 'Tuong', data: { readonly id: "123"; readonly anotherProperty: false; }}
 // case4.data.anotherProperty = false; // Error anotherProperty it is a read-only
 
-
-
-const deepArrA = [1, [2, 3, [4]]] as const;
-const deepArrB = [1, [2, [3, [4]]]] as const;
-
-const deepArrC = [
-  { name: 'Tuong', age: 18, id: 1 },
-  { name: 'Tuong1', age: 11, id: 2 },
-  { name: 'Tuong2', age: 2, id: 3}
-] as const;
-
-export type DeepMutable<T> = { -readonly [P in keyof T]: DeepMutable<T[P]> }; 
-
-const caseDeep1: DeepMutable<typeof deepArrA> = [1, [2, 3, [4]]]; // Result: [1, [2, 3, [4]]]
-const caseDeep2: DeepMutable<typeof deepArrB> = [1, [2, [3, [4]]]] // Result: [1, [2, [3, [4]]]]
-const caseDeep3: DeepMutable<typeof deepArrC> = [
-  { name: 'Tuong', age: 18, id: 1 },
-  { name: 'Tuong1', age: 11, id: 2 },
-  { name: 'Tuong2', age: 2, id: 3}
-];
-caseDeep3[0].age = 18;
-caseDeep3[0].name = 'Tuong';
