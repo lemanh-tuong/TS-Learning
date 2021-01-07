ddf9ea982ec8e4b08fb32fde437062cc58564c40
lemanh-tuong <55147271+lemanh-tuong@users.noreply.github.com>
2021-01-07 22:23:05 +0700

    add FilteredKeys

---
+++ src/TS4.1/FilteredKeys.ts
@@ -0,0 +1,6 @@
+type FilteredKeys<T, U> = { [P in keyof T]: T[P] extends U ? P : never }[keyof T];
+
+type FooItem<T> = T extends number ? { value: T } : undefined;
+
+
+type A = FilteredKeys<{test: 'test', test2: 123, test3: boolean}, string | number>; // Result 'test1' | 'test2';
